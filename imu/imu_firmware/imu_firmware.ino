fabf0d79fcd8a016f525c245df9a1ccb8f0b84c5
Josh Myer <josh@joshisanerd.com>
2017-12-09 10:58:47 -0800

    IMU: checkpoint the last debug code added before giving up on this mode of operation.  The gyro data is just a copy of the accelerometer data, and I have no idea why.  Pulling a different library's example seems to work, so we'll switch to that.

--- imu/imu_firmware/imu_firmware.ino
+++ imu/imu_firmware/imu_firmware.ino
@@ -12,28 +12,80 @@ const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t Ac[3], Gy[3]; // AcX,Ac[1],Ac[2],Tmp,Gy[0],Gy[1],Gy[2]; // MPU sensors
 int16_t Tmp; // I guess they had free silicon?
 
-const char MPU_AXES[3] = { 2,1,0 }; // order of the MPU axes in our frame
+const char MPU_AXES[3] = { 1,2,0 }; // order of the MPU axes in our frame
 
 
 void mpu_setup() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0x00);  // Disable SLEEP, wake up
+  Wire.endTransmission(true);
+  
+  delay(1);
+  
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0x01);  // Enable PLL on x-axis gyro
   Wire.endTransmission(true);  
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6C);  // PWR_MGMT_2 register
+  Wire.write(0x01);  // Disable z-axis gyro...
+  Wire.endTransmission(true);  
+
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x19);  // SMPRT_DIV, p11
+  Wire.write(9);  // Don't need 1kHz sample rate: divide by 1+9=10
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x1A);  // CONFIG, p13
+  Wire.write(0x03);  // Disable FSYNC, set a 44Hz BW DLPF
+  Wire.endTransmission(true);  
+
+  
+  
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+  Serial.println(Wire.read(), HEX);
+  Serial.println(Wire.read(), HEX);  
+
+  Serial.println("0x19:");
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x19);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 4, true);
+  Serial.println(Wire.read(), HEX);
+  Serial.println(Wire.read(), HEX);
+  Serial.println(Wire.read(), HEX);
+  Serial.println(Wire.read(), HEX);
+  
 }
 
 void mpu_read() {
+
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.write(0x43);  // pull Gyro data.
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  Ac[0] =Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  Ac[0]=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  Ac[2]=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  Wire.requestFrom(MPU_addr,6,true);  // request a total of 14 registers
+
   Gy[0]=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   Gy[1]=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   Gy[2]=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,6,true);  // request a total of 14 registers
+  Ac[0]=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  Ac[1]=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  Ac[2]=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+  // Tmp  =Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  
 }
 
 
@@ -45,7 +97,7 @@ void mpu_read() {
 #include "i2c_MAG3110.h"
 MAG3110 mag3110;
 
-const char MAG_AXES[3] = { 1, 0, 2 }; // orientation of mag axes on frame
+const char MAG_AXES[3] = {  0, 1, 2 }; // orientation of mag axes on frame
 
 
 void mag_setup() {
@@ -117,5 +169,5 @@ void loop(){
   Serial.println();
 #undef COMMA
 
-  delay(50);
+  delay(1000);
 }
